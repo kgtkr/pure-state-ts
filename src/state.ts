c237e6eea70ac99572ddef1ee4b2cf743f64e969
kgtkr <kgtkr.jp@gmail.com>
2019-05-02 14:07:35 +0900

    基本

--- src/state.ts
+++ src/state.ts
@@ -2,6 +2,8 @@ import { Overwrite } from "type-zoo";
 
 export type StateFn<S1 extends object, S2 extends object, T> = (state: S1) => [T, S2];
 
+export type Key = string | symbol | number;
+
 export function state<S1 extends object, S2 extends object, T>(fn: StateFn<S1, S2, T>): State<S1, S2, T> {
   return new State(fn);
 }
@@ -40,11 +42,21 @@ export class State<S1 extends object, S2 extends object, T>{
     return new State(s => [s[key], s]);
   }
 
-  static putAll<S extends object>(x: S): State<S, S, null> {
-    return new State(_s => [null, x]);
+  static putAll<S1 extends object, S2 extends object>(x: S2): State<S1, S2, null> {
+    return new State<S1, S2, null>(_s => [null, x]);
   }
 
-  static put<S extends object, K extends string, T>(key: K, x: T): State<S, Overwrite<S, { [P in K]: T }>, null> {
+  static put<S extends object, K extends Key, T>(key: K, x: T): State<S, Overwrite<S, { [P in K]: T }>, null> {
     return new State(s => [null, { ...s, [key]: x }]) as any;
+  }
+
+  static modifyAll<S1 extends object, S2 extends object>(f: (s: S1) => S2): State<S1, S2, null> {
+    return State.getAll<S1>()
+      .then(s => State.putAll(f(s)));
+  }
+
+  static modify<S extends object, K extends keyof S, T>(key: K, f: (s: S[K]) => T): State<S, Overwrite<S, { [P in K]: T }>, null> {
+    return State.get<S, K>(key)
+      .then(s => State.put(key, f(s)));
   }
 }
