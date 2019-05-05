52a55e6b4bc4ea0d7c0b5ebf57e3998eac90b1e6
kgtkr <kgtkr.jp@gmail.com>
2019-05-06 01:18:57 +0900

    test

--- src/state.spec.ts
+++ src/state.spec.ts
@@ -1,10 +1,18 @@
-import { State } from "./state";
+import { State, state } from "./state";
 
 describe("State", () => {
   describe("new", () => {
     it("正常に生成できるか", () => {
       const st = new State<{ x: number }, string>(s => [s.x.toString(), { ...s, x: s.x + 1 }]);
       expect(st.run({ x: 3 })).toEqual(["3", { x: 4 }]);
+    });
+  });
+
+  describe("map", () => {
+    it("map", () => {
+      const st = state<{}, number>(s => [1, s])
+        .map(x => x + 1);
+      expect(st.run({})).toEqual([2, {}]);
     });
   });
 });
