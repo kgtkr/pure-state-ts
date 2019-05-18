9e53045aa4e42ac9c92aa147fcf296c6e832160e
kgtkr <kgtkr.jp@gmail.com>
2019-05-18 17:26:29 +0900

    修正中

--- src/state.spec.ts
+++ src/state.spec.ts
@@ -1,16 +1,16 @@
-import { State } from "./state";
+import { ObjectState } from "./state";
 
 describe("State", () => {
   describe("state", () => {
     it("state", () => {
-      const st = State.state<{ x: number }, string>(s => [s.x.toString(), { ...s, x: s.x + 1 }]);
+      const st = ObjectState.state<{ x: number }, string>(s => [s.x.toString(), { ...s, x: s.x + 1 }]);
       expect(st.run({ x: 3 })).toEqual(["3", { x: 4 }]);
     });
   });
 
   describe("map", () => {
     it("map", () => {
-      const st = State.state<{}, number>(s => [1, s])
+      const st = ObjectState.state<{}, number>(s => [1, s])
         .map(x => x + 1);
       expect(st.run({})).toEqual([2, {}]);
     });
@@ -18,8 +18,8 @@ describe("State", () => {
 
   describe("then", () => {
     it("then", () => {
-      const st = State.state<{ x: number }, string>(s => ["s", s])
-        .then(x => State.state(s => [x + s.x, { ...s, x: s.x + 1 }]));
+      const st = ObjectState.state<{ x: number }, string>(s => ["s", s])
+        .then(x => ObjectState.state(s => [x + s.x, { ...s, x: s.x + 1 }]));
 
       expect(st.run({ x: 1 })).toEqual(["s1", { x: 2 }]);
     });
@@ -27,63 +27,63 @@ describe("State", () => {
 
   describe("getAll", () => {
     it("getAll", () => {
-      const st = State.getAll<{ x: number }>();
+      const st = ObjectState.getAll<{ x: number }>();
       expect(st.run({ x: 2 })).toEqual([{ x: 2 }, { x: 2 }]);
     });
   });
 
   describe("get", () => {
     it("get", () => {
-      const st = State.get<{ x: number, y: string }, "y">("y");
+      const st = ObjectState.get<{ x: number, y: string }, "y">("y");
       expect(st.run({ x: 0, y: "" })).toEqual(["", { x: 0, y: "" }]);
     });
   });
 
   describe("putAll", () => {
     it("putAll", () => {
-      const st = State.putAll<{ x: number }>({ x: 1 });
+      const st = ObjectState.putAll<{ x: number }>({ x: 1 });
       expect(st.run({ x: 0 })).toEqual([null, { x: 1 }]);
     });
   });
 
   describe("put", () => {
     it("put", () => {
-      const st = State.put<{ x: number, y: number }, "x">("x", 2);
+      const st = ObjectState.put<{ x: number, y: number }, "x">("x", 2);
       expect(st.run({ x: 0, y: 1 })).toEqual([null, { x: 2, y: 1 }]);
     });
   });
 
   describe("modifyAll", () => {
     it("modifyAll", () => {
-      const st = State.modifyAll<{ x: number }>(x => ({ ...x, x: x.x * 10 }));
+      const st = ObjectState.modifyAll<{ x: number }>(x => ({ ...x, x: x.x * 10 }));
       expect(st.run({ x: 1 })).toEqual([null, { x: 10 }]);
     });
   });
 
   describe("modify", () => {
     it("modify", () => {
-      const st = State.modify<{ x: number, y: number }, "x">("x", x => x * 10);
+      const st = ObjectState.modify<{ x: number, y: number }, "x">("x", x => x * 10);
       expect(st.run({ x: 1, y: 2 })).toEqual([null, { x: 10, y: 2 }]);
     });
   });
 
   describe("defineAnd", () => {
     it("defineAnd", () => {
-      const st = State.pure<{ x: string, y: string }, null>(null)
-        .defineAnd(() => ({ y: 1, z: "c" }), State.state(s => [s.x.toUpperCase() + (s.y * 10) + s.z, s]));
+      const st = ObjectState.of<{ x: string, y: string }, null>(null)
+        .defineAnd(() => ({ y: 1, z: "c" }), ObjectState.state(s => [s.x.toUpperCase() + (s.y * 10) + s.z, s]));
       expect(st.run({ x: "a", y: "b" })).toEqual(["A10c", { x: "a", y: "b" }]);
     });
   });
 
   describe("passAndThen", () => {
     it("passAndThen", () => {
-      const addAndMul = (a: number) => State.modify<{ b: number, z: string }, "b">("b", b => a + b)
-        .and(State.modify("z", z => z.toUpperCase()))
-        .and(State.get("b"))
+      const addAndMul = (a: number) => ObjectState.modify<{ b: number, z: string }, "b">("b", b => a + b)
+        .and(ObjectState.modify("z", z => z.toUpperCase()))
+        .and(ObjectState.get("b"))
         .map(b => a * b);
 
-      const st = State.pure<{ x: number, y: number, z: string }, null>(null)
-        .and(State.get("x"))
+      const st = ObjectState.of<{ x: number, y: number, z: string }, null>(null)
+        .and(ObjectState.get("x"))
         .passAndThen(x => addAndMul(x), { b: "y", z: "z" });
       expect(st.run({ x: 2, y: 3, z: "a" })).toEqual([10, { x: 2, y: 5, z: "A" }]);
     });
--- src/state.ts
+++ src/state.ts
@@ -1,39 +1,39 @@
 import { Overwrite } from "type-zoo";
-
-export type StateFn<S extends object, T> = (state: S) => [T, S];
+import { right } from "fp-ts/lib/Either";
+export type StateFn<S extends object, A> = (state: S) => [A, S];
 
 type Cast<A, B> = A extends B ? A : B;
 
-type KeyFilter<T, P extends object, _Temp extends keyof P = keyof P> = _Temp extends infer X ? (P[Cast<X, keyof P>] extends T ? X : never) : never;
+type KeyFilter<A, P extends object, _Temp extends keyof P = keyof P> = _Temp extends infer X ? (P[Cast<X, keyof P>] extends A ? X : never) : never;
 type PassObj<S extends object, ThenState extends object> = { [P in keyof ThenState]: KeyFilter<ThenState[P], S> };
 
 // TODO: passAndThenに同じ変数を渡しても対応出来るようにしたい
 
-export class State<S extends object, T>{
-  private constructor(private readonly fn: StateFn<S, T>) {
+export class ObjectState<S extends object, A>{
+  private constructor(private readonly fn: StateFn<S, A>) {
   }
 
-  static state<S extends object, T>(fn: StateFn<S, T>): State<S, T> {
-    return new State(fn);
+  static state<S extends object, A>(fn: StateFn<S, A>): ObjectState<S, A> {
+    return new ObjectState(fn);
   }
 
-  static pure<S extends object, T>(x: T): State<S, T> {
-    return new State(s => [x, s]);
+  static of<S extends object, A>(x: A): ObjectState<S, A> {
+    return new ObjectState(s => [x, s]);
   }
 
-  static empty(): State<{}, null> {
-    return State.pure(null);
+  static empty(): ObjectState<{}, null> {
+    return ObjectState.of(null);
   }
 
-  run(state: S): [T, S] {
+  run(state: S): [A, S] {
     return this.fn(state);
   }
 
-  runEmpty(this: State<{}, T>): T {
+  runEmpty(this: ObjectState<{}, A>): A {
     return this.fn({})[0];
   }
 
-  eval(state: S): T {
+  eval(state: S): A {
     return this.fn(state)[0];
   }
 
@@ -41,52 +41,52 @@ export class State<S extends object, T>{
     return this.fn(state)[1];
   }
 
-  map<P>(f: (x: T) => P): State<S, P> {
-    return new State(s1 => {
+  map<P>(f: (x: A) => P): ObjectState<S, P> {
+    return new ObjectState(s1 => {
       const [res, s2] = this.fn(s1);
       return [f(res), s2];
     });
   }
 
-  then<R>(f: (x: T) => State<S, R>): State<S, R> {
-    return new State<S, R>(s => {
+  then<R>(f: (x: A) => ObjectState<S, R>): ObjectState<S, R> {
+    return new ObjectState<S, R>(s => {
       const [a, newState] = this.fn(s);
       return f(a).fn(newState);
     });
   }
 
-  and<R>(s: State<S, R>): State<S, R> {
+  and<R>(s: ObjectState<S, R>): ObjectState<S, R> {
     return this.then(() => s);
   }
 
-  static getAll<S extends object>(): State<S, S> {
-    return new State(s => [s, s]);
+  static getAll<S extends object>(): ObjectState<S, S> {
+    return new ObjectState(s => [s, s]);
   }
 
-  static get<S extends object, K extends keyof S>(key: K): State<S, S[K]> {
-    return new State(s => [s[key], s]);
+  static get<S extends object, K extends keyof S>(key: K): ObjectState<S, S[K]> {
+    return new ObjectState(s => [s[key], s]);
   }
 
-  static putAll<S extends object>(x: S): State<S, null> {
-    return new State<S, null>(_s => [null, x]);
+  static putAll<S extends object>(x: S): ObjectState<S, null> {
+    return new ObjectState<S, null>(_s => [null, x]);
   }
 
-  static put<S extends object, K extends keyof S>(key: K, x: S[K]): State<S, null> {
-    return new State(s => [null, { ...s, [key]: x }]);
+  static put<S extends object, K extends keyof S>(key: K, x: S[K]): ObjectState<S, null> {
+    return new ObjectState(s => [null, { ...s, [key]: x }]);
   }
 
-  static modifyAll<S extends object>(f: (s: S) => S): State<S, null> {
-    return State.getAll<S>()
-      .then(s => State.putAll(f(s)));
+  static modifyAll<S extends object>(f: (s: S) => S): ObjectState<S, null> {
+    return ObjectState.getAll<S>()
+      .then(s => ObjectState.putAll(f(s)));
   }
 
-  static modify<S extends object, K extends keyof S>(key: K, f: (s: S[K]) => S[K]): State<S, null> {
-    return State.get<S, K>(key)
-      .then(s => State.put(key, f(s)));
+  static modify<S extends object, K extends keyof S>(key: K, f: (s: S[K]) => S[K]): ObjectState<S, null> {
+    return ObjectState.get<S, K>(key)
+      .then(s => ObjectState.put(key, f(s)));
   }
 
-  defineAnd<D extends object, R>(valueFn: (x: T) => D, f: State<Overwrite<S, D>, R>): State<S, R> {
-    return new State<S, R>(s => {
+  defineAnd<D extends object, R>(valueFn: (x: A) => D, f: ObjectState<Overwrite<S, D>, R>): ObjectState<S, R> {
+    return new ObjectState<S, R>(s => {
       const [x1, newState1] = this.fn(s);
       const value = valueFn(x1);
 
@@ -119,8 +119,8 @@ export class State<S extends object, T>{
     });
   }
 
-  passAndThen<ThenState extends object, R>(f: (x: T) => State<ThenState, R>, pass: PassObj<S, ThenState>): State<S, R> {
-    return new State<S, R>(s => {
+  passAndThen<ThenState extends object, R>(f: (x: A) => ObjectState<ThenState, R>, pass: PassObj<S, ThenState>): ObjectState<S, R> {
+    return new ObjectState<S, R>(s => {
       const [a, newState] = this.fn(s);
       const passState: any = {};
       for (let key of Object.keys(pass)) {
